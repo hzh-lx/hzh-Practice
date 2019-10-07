ee0c471a774df9a3090e3378405f55e37ed536af
123 胡致航 <hu694014352@163.com>
2019-10-07 09:58:55 +0800

    很开心，完成了第一次任务

--- Gitshow/Program.cs
+++ Gitshow/Program.cs
@@ -6,12 +6,23 @@ namespace Gitshow
     {
         static void Main(string[] args)
         {
-            int age = 18;
-            string name = "hzh";
-            Console.WriteLine("huhfghfb".Substring(3,3));
-            Console.Read();
+            int random = new Random().Next(1000);
+            Console.WriteLine("请输入你所要猜的字符只有5次机会,数字范围在0到1000以内的整数。");
+            for (int i = 0; i < 5; i++)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (random == number)
+                { Console.WriteLine("恭喜你答对了"); }
+                else
+                { if (i != 4)
+                    { Console.WriteLine($"不好意思你答错了，你还有{4 - i}次机会"); }
+                else
+                    { Console.WriteLine($"不好意思你输了,正确数值为{random}"); }
+                }
+             
+            }
 
-            
+
         }
     }
 }
